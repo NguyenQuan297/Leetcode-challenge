b83b2f06deccf0e58a669e625aec7b9686b8efff
Quan Nguyen with love <justinvu3689@gmail.com>
2026-04-02 16:47:39 -0500

    Add sorting/problem389: Find the Difference (Python, C++, README)

--- sorting/README.md
+++ sorting/README.md
@@ -12,3 +12,4 @@ LeetCode problems related to **sorting / hashing / grouping by key**.
 | 164 | Maximum Gap | [problem164/](problem164/) |
 | 268 | Missing Number | [problem268/](problem268/) |
 | 324 | Wiggle Sort II | [problem324/](problem324/) |
+| 389 | Find the Difference | [problem389/](problem389/) |
---
+++ sorting/problem389/problem389.cpp
@@ -0,0 +1,17 @@
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        char x = 0;
+        for (char c : s) {
+            x ^= c;
+        }
+        for (char c : t) {
+            x ^= c;
+        }
+        return x;
+    }
+};
